8e8f6bb3a19a6c1fc1d9916c6c6a2dffa419442e
Calvinbullock <calvinnbullock@gmail.com>
2022-06-13 11:04:08 -0600

    Team09 will find a nd print the sum of the list

--- Team09.py
+++ Team09.py
@@ -7,3 +7,10 @@ while new_num != 0:
 
     if new_num != 0:
         num_list.append(new_num)
+
+# This code block determains the sum of num_list
+sum = 0
+for i in num_list: 
+    sum += i
+
+print(f"The sum is: {sum}")
