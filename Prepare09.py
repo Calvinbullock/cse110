35b5bc9c4e7571cbc66e592997a2776b25089fc8
Calvinbullock <calvinnbullock@gmail.com>
2022-06-13 11:01:28 -0600

    fixed the code that adds number to the list.

--- Prepare09.py
+++ Prepare09.py
@@ -5,9 +5,9 @@ i = 0
 
 while new_name.upper() != "END":
     new_name = input("Type the name of a friend: ")
-    names.append(new_name)
 
     if new_name.upper() != "END":
+        names.append(new_name)
         i += 1
 
 for i in names:
