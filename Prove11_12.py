e895ff9caf4126887aa47475593ac533afd60ac2
Calvinbullock <calvinnbullock@gmail.com>
2022-07-08 10:49:47 -0600

    made some verable name changes

--- Prove11_12.py
+++ Prove11_12.py
@@ -1,22 +1,30 @@
 
-intrest_year = int(input("Enter the year of interest: "))
+choice = int(input("Would you like to input a 1.year or a 2.country? ")) 
+
+if choice == 1:
+    spec_yr = int(input("Enter the year of interest: "))
+
+    spec_yr_min_life_exp = 100.0
+    spec_yr_min_life_ctry = 0
+    spec_yr_max_life_exp = 0.00
+    spec_yr_max_life_ctry = 0
+
+elif choice == 2:
+    spec_ctry = int(input("Enter the country of interest: "))
+
 
 average_life_ex = 0
 average_count = 0
 
 min_life_exp = 100.0
 min_life_country = 0
-min_life_year = 0
+min_life_yr = 0
 
 max_life_exp = 0.00
 max_life_country = 0
-max_life_year = 0
+max_life_yr = 0
 
-intrest_min_life_exp = 100.0
-intrest_min_life_country = 0
 
-intrest_max_life_exp = 0.00
-intrest_max_life_country = 0
 
 with open("life-expectancy.csv") as life_file:
     next(life_file)
@@ -26,39 +34,57 @@ with open("life-expectancy.csv") as life_file:
         year = int(year)
         life_expectancy = float(life_expectancy)
 
+        # finds the country and year with the smallest life expectancy in the whole data set
         if min_life_exp > life_expectancy:
             min_life_exp = life_expectancy
             min_life_country = country
-            min_life_year = year
+            min_life_yr = year
 
+         # finds the country and year with the greatest life expectancy in the whole data set
         if max_life_exp < life_expectancy:
             max_life_exp = life_expectancy
             max_life_country = country
-            max_life_year = year
-        
-        if year == intrest_year:
-            average_life_ex += life_expectancy
-            average_count += 1
+            max_life_yr = year
 
-            if intrest_min_life_exp > life_expectancy:
-                intrest_min_life_exp = life_expectancy
-                intrest_min_life_country = country
+        if choice == 1:
+            # Finds the greatest and least life expectancy with a matching year to user input
+            if year == spec_yr:
+                average_life_ex += life_expectancy
+                average_count += 1
 
-            if intrest_max_life_exp < life_expectancy:
-                intrest_max_life_exp = life_expectancy
-                intrest_max_life_country = country
+                if intrest_min_life_exp > life_expectancy:
+                    spec_yr_min_life_exp = life_expectancy
+                    spec_yr_min_life_ctry = country
+
+                if intrest_max_life_exp < life_expectancy:
+                    spec_yr_max_life_exp = life_expectancy
+                    spec_yr_max_life_ctry = country
+
+        else:
+            # Finds the greatest and least life expectancy with a matching country to user input
+            if year == spec_ctry:
+                average_life_ex += life_expectancy
+                average_count += 1
+
+                if intrest_min_life_exp > life_expectancy:
+                    intrest_min_life_exp = life_expectancy
+                    intrest_min_life_country = country
+
+                if intrest_max_life_exp < life_expectancy:
+                    intrest_max_life_exp = life_expectancy
+                    intrest_max_life_country = country
             
     average_life_ex = average_life_ex / average_count
 
 print()
-print(f"The overall max life expectancy is: {max_life_exp} from {max_life_country} in {max_life_year}")
-print(f"The overall min life expectancy is: {min_life_exp} from {min_life_country} in {min_life_year}")
+print(f"The overall max life expectancy is: {max_life_exp} from {max_life_country} in {max_life_yr}")
+print(f"The overall min life expectancy is: {min_life_exp} from {min_life_country} in {min_life_yr}")
 
 print()
-print(f"For the year {intrest_year}")
+print(f"For the year {spec_yr}")
 print(f"The average life expectancy across all countries was {average_life_ex:.2f}")
-print(f"The max life expectancy was in {intrest_max_life_country} with {intrest_max_life_exp}")
-print(f"The min life expectancy was in {intrest_min_life_country} with {intrest_min_life_exp}")
+print(f"The max life expectancy was in {spec_yr_max_life_ctry} with {spec_yr_max_life_exp}")
+print(f"The min life expectancy was in {spec_yr_min_life_ctry} with {spec_yr_min_life_exp}")
 
 
 # TODO INFO
